096a3f4677ef015859ee98610bf86f7784d184b4
tungduong0708 <tungduong0708@gmail.com>
2025-08-28 08:13:40 +0700

    fix: move firebase config into .env

--- backend/src/config/firebase.js
+++ backend/src/config/firebase.js
@@ -1,7 +1,8 @@
 const admin = require('firebase-admin');
 
 // TODO: Add your service account key
-const serviceAccount = require('../../serviceAccountKey.json');
+// const serviceAccount = require('../../serviceAccountKey.json');
+const serviceAccount = JSON.parse(process.env.FIREBASE_CONFIG);
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount)
