7701173544c7d82c3c76e3837da5f435a6bff008
tungduong0708 <tungduong0708@gmail.com>
2025-08-30 10:54:53 +0700

    feat: temporary turn off auth

--- backend/src/routes/cameraRoutes.js
+++ backend/src/routes/cameraRoutes.js
@@ -9,7 +9,7 @@ const authMiddleware = require('../middleware/authMiddleware');
 const router = express.Router();
 
 // Protect all routes in this file
-router.use(authMiddleware);
+// router.use(authMiddleware);
 
 router.post('/recognize-details', recognizeFoodDetails);
 router.post('/barcode', recognizeBarcode);
