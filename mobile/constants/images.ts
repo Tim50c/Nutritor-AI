72982e002528cda717d96e2a5a72a8506b777c4f
ltdsword <ltdsp1514@gmail.com>
2025-09-10 14:17:55 +0700

    fix: image

--- mobile/constants/images.ts
+++ mobile/constants/images.ts
@@ -4,13 +4,10 @@ import default_avatar from '@/assets/images/default-avatar.png';
 import pho from '@/assets/images/pho.png';
 import chicken_salad from '@/assets/images/chicken_salad.png';
 import avocado_toast from '@/assets/images/avocado_toast.png';
-import fruit_smoothie from '@/assets/images/fruit_smoothie.png';
 import grilled_salmon from '@/assets/images/grilled_salmon.png';
 import veggie_stirfry from '@/assets/images/veggie_stirfry.png';
-import pancakes from '@/assets/images/pancakes.png';
 import quinoa_bowl from '@/assets/images/quinoa_bowl.png';
 import yogurt_parfait from '@/assets/images/yogurt_parfait.png';
-import turkey_sandwich from '@/assets/images/turkey_sandwich.png';
 import emptyScreen from '@/assets/images/empty_screen.png';
 
 export const images = {
@@ -20,12 +17,9 @@ export const images = {
   pho,
   chicken_salad,
   avocado_toast,
-  fruit_smoothie,
   grilled_salmon,
   veggie_stirfry,
-  pancakes,
   quinoa_bowl,
   yogurt_parfait,
-  turkey_sandwich,
   emptyScreen
 };
